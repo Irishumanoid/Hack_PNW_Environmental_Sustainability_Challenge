9193e38acef3aaf7868dd099a514fd3869344698
Chistopher Huntwork <christopher@huntwork.net>
2023-06-03 16:51:40 -0700

    added my method

--- bindings/test.py
+++ bindings/test.py
@@ -2,3 +2,4 @@ import requests
 from pprint import pprint
 
 pprint(requests.get("https://www.strava.com/api/v3/athlete", headers={"Authorization": "Bearer f866f7ab7692ae8e615bd6466547e5fe4cefd7a6"}).json())
+pprint(requests.get("https://www.strava.com/api/v3/segments/explore?bounds=0.0,0.0,0.0,0.0", headers={"Authorization": "Bearer f866f7ab7692ae8e615bd6466547e5fe4cefd7a6"}).json())
