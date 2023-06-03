3906aa693599a702a46a2f14bb13aa04bb50f514
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2023-06-03 13:28:54 -0700

    strava auth

--- bindings/strava_poll.py
+++ bindings/strava_poll.py
@@ -1,2 +1,26 @@
 import flask
+from flask import Blueprint, request
+import requests
 
+auth_url = "https://www.strava.com/oauth/token"
+activites_url = "https://www.strava.com/api/v3/athlete/activities"
+
+payload = {
+    'client_id': "xxxx",
+    'client_secret': 'xxxx',
+    'refresh_token': 'xxxx',
+    'grant_type': "refresh_token",
+    'f': 'json'
+}
+
+bp = Blueprint("strava", __name__)
+
+@bp.route("/athlete", methods = ['GET'])
+def get_athlete():
+    if request.method == 'GET':
+        res = requests.post(auth_url, data=payload, verify=False)
+        access_token = res.json()['access_token']
+        header = {'Authorization': 'Bearer ' + access_token}
+        param = {'per_page': 200, 'page': 1}
+        person_data = requests.get(activites_url, headers=header, params=param).json()
+        return person_data
