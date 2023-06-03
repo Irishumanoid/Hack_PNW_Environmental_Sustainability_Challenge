d8f9afe80d01e29233deb52314913e91a9ac06c2
Ben <69478311+mojo626@users.noreply.github.com>
2023-06-03 11:50:26 -0700

    fetch should work now

--- src/App.js
+++ src/App.js
@@ -11,12 +11,13 @@ function App() {
   function getData() {
     axios({
       method: "GET",
-      url:"/",
+      url:"/profile",
     })
     .then((response) => {
       const res =response.data
       setProfileData(({
-        profile_name: res}))
+        profile_name: res.name,
+        about_me: res.about}))
     }).catch((error) => {
       if (error.response) {
         console.log(error.response)
@@ -24,6 +25,12 @@ function App() {
         console.log(error.response.headers)
         }
     })}
+
+    async function fetchData()
+    {
+      let json = await (await fetch("http://chrissytopher.com:5000/profiles")).json();
+      console.log(json);
+    }
     //end of new line 
 
   return (
@@ -43,7 +50,7 @@ function App() {
         </a>
 
         {/* new line start*/}
-        <p>To get your profile details: </p><button onClick={getData}>Click me</button>
+        <p>To get your profile details: </p><button onClick={fetchData}>Click me</button>
         {profileData && <div>
               <p>Profile name: {profileData.profile_name}</p>
               <p>About me: {profileData.about_me}</p>
