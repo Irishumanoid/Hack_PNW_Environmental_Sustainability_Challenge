fe3eef54ebc5b32ca28ef5813538567e88669f49
Chistopher Huntwork <christopher@huntwork.net>
2023-06-03 12:20:59 -0700

    api test

--- POC.py
+++ POC.py
@@ -1,16 +1,21 @@
 import json
 import flask
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
+@cross_origin()
 def simple():
     return 'test hello Iris'
 
 
 @app.route('/users', methods=["GET"])
+@cross_origin()
 def users():
     print("users endpoint reached...")
     with open("users.json", "r") as f:
@@ -20,6 +25,13 @@ def users():
             "pets": ["hamster"]
         })
         return flask.jsonify(data)
+
+@app.route('/trails', methods=["POST"])
+def trails():
+    content = request.json
+    print(content['running_speed'])
+    return jsonify({"trail-name": "def real trail :wink_emoji:", "trail-difficulty": "10+ idk"})
+
 
 if __name__ == "__main__":
     app.run("localhost", 5000)
