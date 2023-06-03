ce80b8a9566e4e3ae71c0b46e2a31fffdf34dd96
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2023-06-03 11:56:56 -0700

    changes

--- POC.py
+++ POC.py
@@ -1,12 +1,25 @@
-from flask import Flask
+import json
+import flask
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def simple():
-    return f'test hello crystal'
+    return 'test hello crystal'
 
+
+@app.route('/users', methods=["GET"])
+def users():
+    print("users endpoint reached...")
+    with open("users.json", "r") as f:
+        data = json.load(f)
+        data.append({
+            "username": "user4",
+            "pets": ["hamster"]
+        })
+        return flask.jsonify(data)
 
 if __name__ == "__main__":
     app.run("localhost", 5000)
