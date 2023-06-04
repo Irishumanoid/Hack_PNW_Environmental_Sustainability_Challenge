3415f3c41c4d0d4f1220a4209dc561e8d7696ba4
Ben <69478311+mojo626@users.noreply.github.com>
2023-06-04 12:02:08 -0700

    frontendchanges

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,6 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import {Link} from "react-router-dom";
 import PrettyLink from "./PrettyLink"
 import Stack from '@mui/material/Stack';
--- frontend/src/routes/Landing.js
+++ frontend/src/routes/Landing.js
@@ -11,7 +11,7 @@ import Box from "@mui/material/Box";
 export default function Landing(props) {
 
     const REACT_APP_CLIENT_ID = 108420;
-    const redirectUrl = "http://localhost:3000/traildata";
+    const redirectUrl = "http://localhost:3000/app";
     const scope = "read"
 
     const location = `http://www.strava.com/oauth/authorize?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${redirectUrl}&approval_prompt=force&scope=${scope}`
