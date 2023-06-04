6d61a2d4ec000a19fc504a17b94f5d6612d13bd6
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2023-06-04 12:14:00 -0700

    cry

--- bindings/recommend.py
+++ bindings/recommend.py
@@ -7,33 +7,38 @@ from flask_cors import cross_origin
 post_bp = Blueprint("suggestions", __name__)
 
 
-def json_to_list(file, key:str) -> list[str]:
+def json_to_val(file, key:str) -> str:
     data_map = ""
     with open(file, 'r') as f:
         data_map = json.loads(f.read())
-    return data_map[key]
+    return data_map[0][key]
 
-def parse_user(user_json): 
-    raw_json = json.loads(user_json)
+
+def vals_to_list(file):
     runs = []
-    for raw_run in raw_json:
+    for raw in json.loads(file):
         run = {}
-        run["distance"] = raw_run.get("distance")
-        run["moving_time"] = raw_run.get("moving_time")
-        run["total_elevation_gain"] = raw_run.get("total_elevation_gain")
+        run['distance'] = raw.get('distance')
+        run['moving_time'] = raw.get('moving_time')
+        run['total_elevation_gain'] = raw.get('total_elevation_gain')
         runs.append(run)
     return runs
 
-print(json.dumps(parse_user(open("bindings/activity_data.json").read())))
-# print(json_to_list("bindings/dummy_data.json", "biggest_ride_distance"))
+print(json.dumps(vals_to_list(open("bindings/activity_data.json").read())))
 
 
 def suggest_route(routes:list[str], param:str) -> list[str]:
+    distances = []
+    for run in routes.keys():
+        if run == param:
+            distances.append(run[param])
+    
     min_len, max_len = min(routes), max(routes)
-    possible = json_to_list(sp.get_relevant_segments())
+    possible = json_to_val(sp.get_relevant_segments_inner())
 
     valid = [i for i, entry in possible if (entry[param] > min_len and entry[param] < max_len)]
     request.data += valid
+    return request.data
 
 
 
