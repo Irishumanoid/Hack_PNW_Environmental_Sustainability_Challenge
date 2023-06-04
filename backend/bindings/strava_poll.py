35fe3d6a61f0e4984ed5a5570133bc9a3a424325
Chistopher Huntwork <christopher@huntwork.net>
2023-06-04 13:32:51 -0700

    inowgf

--- backend/bindings/strava_poll.py
+++ backend/bindings/strava_poll.py
@@ -25,7 +25,7 @@ def get_athlete():
         athlete["city"] = raw_athlete["city"]
         athlete["state"] = raw_athlete["state"]
         athlete["username"] = raw_athlete["username"]
-        athlete["score"] = recommend.get_user_score(json.dumps(recommend.vals_to_list(open("bindings/activity_data.json").read())))
+        athlete["score"] = recommend.get_user_score(recommend.vals_to_list(open("bindings/activity_data.json").read()))
         if "athelete_type" in raw_athlete:
             athlete["type"] = raw_athlete["athlete_type"]
         return "💀"+json.dumps(athlete)
@@ -52,21 +52,23 @@ def get_relavant_segments():
         #             segment_cool = make_api_call("segments/"+str(segment_id), request.args.get('token'))
         #             relavant_segments_cool.append(segment_cool)
         relavant_segments = get_relavant_segments_inner(bounding_width, bounding_center, request.args.get('token'))
-        return "💀" + json.dumps(userify_data(relavant_segments.get("segments")))
+        return "💀" + json.dumps(userify_data(relavant_segments))
 
 #I can't be bothered to pass this in somehow, set it before calling routes.sort
 user_score = 0
 def sortRouteFunction(route):
-    return math.sqrt(math.pow(user_score, 2)+math.pow(recommend.get_single_route_difficulty(route), 2))
+    return math.sqrt(math.pow(user_score, 2)+math.pow(route["difficulty"], 2))
 
 def get_relavant_segments_inner(bounding_width, bounding_center, token):
     global user_score
     #https://developers.strava.com/docs/reference/#api-Segments-exploreSegments:~:text=%5Bsouthwest%20corner%20latitutde%2C%20southwest%20corner%20longitude%2C%20northeast%20corner%20latitude%2C%20northeast%20corner%20longitude%5D
     bounding_rect = [bounding_center[0]-bounding_width/2, bounding_center[1]-bounding_width/2, bounding_center[0]+bounding_width/2, bounding_center[1]+bounding_width/2]
     bounds_string = "%f,%f,%f,%f" % (bounding_rect[0], bounding_rect[1], bounding_rect[2], bounding_rect[3])
-    routes = make_api_call("segments/explore?bounds="+bounds_string, token)
-    user_score = recommend.get_user_score(json.dumps(recommend.vals_to_list(open("bindings/activity_data.json").read())))
-    routes.sort(key=sortRouteFunction, reversed=True)
+    routes = make_api_call("segments/explore?bounds="+bounds_string, token).get("segments")
+    for segment in routes:
+        segment["difficulty"] = recommend.get_single_route_difficulty(segment)
+    user_score = recommend.get_user_score(recommend.vals_to_list(open("bindings/activity_data.json").read()))
+    routes.sort(key=sortRouteFunction, reverse=True)
     return routes
 
 def userify_data(segments):
@@ -79,16 +81,12 @@ def userify_data(segments):
         new_segment["name"] = segment.get("name")
         new_segment["distance"] = segment.get("distance")
         new_segment["elevation"] = segment.get("elev_difference")
-        new_segment["difficulty"] = get_segment_difficulty(segment)
+        new_segment["difficulty"] = segment.get("difficulty")
         new_segment["start_location"] = segment.get("start_latlng")
         new_segment["end_location"] = segment.get("end_latlng")
         new_segments.append(new_segment)
     print(new_segments)
     return new_segments
-
-def get_segment_difficulty(segment):
-    #TODO do this!
-    return 0.0
 
 def make_api_call(path, token):
     return requests.get("https://www.strava.com/api/v3/" + path, headers={"Authorization": "Bearer " + token}).json()
