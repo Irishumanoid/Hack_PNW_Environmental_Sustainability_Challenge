d28847be0bab2e62e5a47a1bc3146858c7da27e0
Chistopher Huntwork <christopher@huntwork.net>
2023-06-03 17:23:03 -0700

    works now

--- bindings/strava_poll.py
+++ bindings/strava_poll.py
@@ -21,14 +21,14 @@ strava_bp = Blueprint("strava", __name__)
 @cross_origin()
 def get_athlete():
     if request.method == 'GET':
-        make_api_call("/athlete", request.args.get("token"))
+        return make_api_call("/athlete", request.args.get("token"))
     
 
 @strava_bp.route("/athlete/{id}/stats", methods = ['GET'])
 @cross_origin()
 def get_athlete_stats():
     if request.method == 'GET':
-        make_api_call('athlete/{id}/stats', request.args.get('token'))
+        return make_api_call('athlete/{id}/stats', request.args.get('token'))
 
     
 @strava_bp.route("/get_relavant_segments", methods = ['GET'])
@@ -39,13 +39,12 @@ def get_relavant_segments():
         bounding_center = [json.loads(request.args.get("bounding_center_lat")), json.loads(request.args.get("bounding_center_long"))]
         #https://developers.strava.com/docs/reference/#api-Segments-exploreSegments:~:text=%5Bsouthwest%20corner%20latitutde%2C%20southwest%20corner%20longitude%2C%20northeast%20corner%20latitude%2C%20northeast%20corner%20longitude%5D
         bounding_rect = [bounding_center[0]-bounding_width/2, bounding_center[1]-bounding_width/2, bounding_center[0]+bounding_width/2, bounding_center[1]+bounding_width/2]
-        api_instance = swagger_client.SegmentsApi()
-        relavant_segments_lame = api_instance.explore_segments(bounding_rect)
+        relavant_segments_lame = make_api_call("segments/explore?bounds=%f,%f,%f,%f" % (bounding_rect[0], bounding_rect[1], bounding_rect[2], bounding_rect[3]), request.args.get('token'))
         relavant_segments_cool = []
-        for segment in relavant_segments_lame:
+        pprint(relavant_segments_lame)
+        for segment in relavant_segments_lame.get("segments"):
             segment_id = segment.id
-            api_instance = swagger_client.SegmentsApi()
-            segment_cool = api_instance.getSegmentById(segment_id)
+            segment_cool = make_api_call("segments/"+segment_id, request.args.get('token'))
             relavant_segments_cool.append(segment_cool)
         return jsonify(relavant_segments_cool)
 
